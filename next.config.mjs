fc43ed94861270edb9101671123c8fd23fd742b7
Orlandini Sebastien <sebastien.orlandini@gmail.com>
2024-07-23 15:03:54 +0200

    timeout

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,23 @@ const nextConfig = {
 		domains: ['i.annihil.us'],
 	},
 	staticPageGenerationTimeout: 1000,
+	async headers() {
+		return [
+			{
+				source: '/(.*)', // Applique à toutes les pages
+				headers: [
+					{
+						key: 'Cache-Control',
+						value: 's-maxage=10, stale-while-revalidate', // Ajustez selon vos besoins
+					},
+					{
+						key: 'Keep-Alive',
+						value: 'timeout=59', // Ajustez selon vos besoins
+					},
+				],
+			},
+		];
+	},
 };
 
 export default nextConfig;
