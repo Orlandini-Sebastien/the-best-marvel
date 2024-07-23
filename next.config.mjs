077e15c741e4d84e830432b6787f4332d9298537
Orlandini Sebastien <sebastien.orlandini@gmail.com>
2024-07-23 09:08:14 +0200

    again

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-	  domains: ['i.annihil.us'],
+		domains: ['i.annihil.us'],
 	},
-	async headers() {
-	  return [
-		{
-		  source: "/api/(.*)",
-		  headers: [
-			{
-			  key: "Keep-Alive",
-			  value: "timeout=600", // 10 minutes
-			},
-		  ],
-		},
-	  ];
-	},
-  };
-  
-  export default nextConfig;
-  
+};
+
+export default nextConfig;
